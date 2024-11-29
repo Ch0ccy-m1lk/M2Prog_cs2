5132b9fa9d41e8a5835c2b7819ae70571ac0a175
Ch0ccy-m1lk <louellahanen@outlook.com>
2024-11-29 10:25:25 +0100

    Rijschool

---
+++ Rijschool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rijschool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto
+            RijLeraar
+            Student
+            LesPakket
+            RijTest
+            TheorieTest
+            Dag
+            LesUur
+
+
+
+        }
+    }
+}
